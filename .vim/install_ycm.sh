96270707c3ae4e30c69a6b68f7c3f58cbfb23bae
hyliang96 <hyliang96@hotmail.com>
2023-02-22 01:43:21 +0800

    logout push mac-MBP.local 2023-02-22 01:43:21

--- .vim/install_ycm.sh
+++ .vim/install_ycm.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 if [ "$(uname)" = "Darwin" ]; then
-    # 系统的python3
-    /usr/bin/python3 ~/.vim/plugged/YouCompleteMe/install.py
+    # 系统的python3: 版本太低了
+    # /usr/bin/python3 ~/.vim/plugged/YouCompleteMe/install.py
     # brew的python3
-    # /usr/local/bin/python3  ~/.vim/plugged/YouCompleteMe/install.py
+    /usr/local/bin/python3  ~/.vim/plugged/YouCompleteMe/install.py
+    # mac系统用conda的python3无法编译ycm
 else
     python3 ~/.vim/plugged/YouCompleteMe/install.py # --clang-completer
 fi
